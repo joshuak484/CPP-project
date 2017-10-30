63026e3fe8c04975de1422f113fd7ff9fa759d9b
Josh Kramer <joshuak484@gmail.com>
2017-10-30 16:04:05 -0700

    adding boards file

--- src/SudokuBoardMaker.cpp
+++ src/SudokuBoardMaker.cpp
@@ -10,6 +10,99 @@ using namespace std;
 
 
 
+void defaultBoard(int**& sudoku)
+{
+  sudoku[0][0] = 1;
+  sudoku[0][1] = 8;
+  sudoku[0][2] = 7;
+  sudoku[0][3] = 6;
+  sudoku[0][4] = 4;
+  sudoku[0][5] = 2;
+  sudoku[0][6] = 3;
+  sudoku[0][7] = 9;
+  sudoku[0][8] = 5;
+  
+  sudoku[1][0] = 3;
+  sudoku[1][1] = 4;
+  sudoku[1][2] = 5;
+  sudoku[1][3] = 7;
+  sudoku[1][4] = 1;
+  sudoku[1][5] = 9;
+  sudoku[1][6] = 8;
+  sudoku[1][7] = 2;
+  sudoku[1][8] = 6;
+  
+  sudoku[2][0] = 6;
+  sudoku[2][1] = 9;
+  sudoku[2][2] = 2;
+  sudoku[2][3] = 8;
+  sudoku[2][4] = 5;
+  sudoku[2][5] = 3;
+  sudoku[2][6] = 4;
+  sudoku[2][7] = 7;
+  sudoku[2][8] = 1;
+  
+  sudoku[3][0] = 8;
+  sudoku[3][1] = 3;
+  sudoku[3][2] = 1;
+  sudoku[3][3] = 2;
+  sudoku[3][4] = 7;
+  sudoku[3][5] = 5;
+  sudoku[3][6] = 9;
+  sudoku[3][7] = 6;
+  sudoku[3][8] = 4;
+  
+  sudoku[4][0] = 7;
+  sudoku[4][1] = 2;
+  sudoku[4][2] = 9;
+  sudoku[4][3] = 1;
+  sudoku[4][4] = 6;
+  sudoku[4][5] = 4;
+  sudoku[4][6] = 5;
+  sudoku[4][7] = 8;
+  sudoku[4][8] = 3;
+  
+  sudoku[5][0] = 5;
+  sudoku[5][1] = 6;
+  sudoku[5][2] = 4;
+  sudoku[5][3] = 9;
+  sudoku[5][4] = 3;
+  sudoku[5][5] = 8;
+  sudoku[5][6] = 2;
+  sudoku[5][7] = 1;
+  sudoku[5][8] = 7;
+  
+  sudoku[6][0] = 2;
+  sudoku[6][1] = 7;
+  sudoku[6][2] = 3;
+  sudoku[6][3] = 5;
+  sudoku[6][4] = 8;
+  sudoku[6][5] = 6;
+  sudoku[6][6] = 1;
+  sudoku[6][7] = 4;
+  sudoku[6][8] = 9;
+  
+  sudoku[7][0] = 4;
+  sudoku[7][1] = 1;
+  sudoku[7][2] = 8;
+  sudoku[7][3] = 3;
+  sudoku[7][4] = 9;
+  sudoku[7][5] = 7;
+  sudoku[7][6] = 6;
+  sudoku[7][7] = 5;
+  sudoku[7][8] = 2;
+  
+  sudoku[8][0] = 9;
+  sudoku[8][1] = 5;
+  sudoku[8][2] = 6;
+  sudoku[8][3] = 4;
+  sudoku[8][4] = 2;
+  sudoku[8][5] = 1;
+  sudoku[8][6] = 7;
+  sudoku[8][7] = 3;
+  sudoku[8][8] = 8;
+}
+
 void getAvaliableSlots(vector<int*>& vect, int** board, int n)
 {
   vect.clear();// clear vector
@@ -100,6 +193,19 @@ bool isValid(int** board)
   return correctness;
 }
 
+void printSudoku(int** board)
+{
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < 9; j++)
+    {
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+/*
 int main()
 {
   int** sudoku;
@@ -116,111 +222,15 @@ int main()
       for(int j=0;j<9;j++)
         sudoku[i][j] = rand() % 9 + 1;
   
-  /*sudoku[0][0] = 1;
-  sudoku[0][1] = 8;
-  sudoku[0][2] = 7;
-  sudoku[0][3] = 6;
-  sudoku[0][4] = 4;
-  sudoku[0][5] = 2;
-  sudoku[0][6] = 3;
-  sudoku[0][7] = 9;
-  sudoku[0][8] = 5;
   
-  sudoku[1][0] = 3;
-  sudoku[1][1] = 4;
-  sudoku[1][2] = 5;
-  sudoku[1][3] = 7;
-  sudoku[1][4] = 1;
-  sudoku[1][5] = 9;
-  sudoku[1][6] = 8;
-  sudoku[1][7] = 2;
-  sudoku[1][8] = 6;
-  
-  sudoku[2][0] = 6;
-  sudoku[2][1] = 9;
-  sudoku[2][2] = 2;
-  sudoku[2][3] = 8;
-  sudoku[2][4] = 5;
-  sudoku[2][5] = 3;
-  sudoku[2][6] = 4;
-  sudoku[2][7] = 7;
-  sudoku[2][8] = 1;
-  
-  sudoku[3][0] = 8;
-  sudoku[3][1] = 3;
-  sudoku[3][2] = 1;
-  sudoku[3][3] = 2;
-  sudoku[3][4] = 7;
-  sudoku[3][5] = 5;
-  sudoku[3][6] = 9;
-  sudoku[3][7] = 6;
-  sudoku[3][8] = 4;
-  
-  sudoku[4][0] = 7;
-  sudoku[4][1] = 2;
-  sudoku[4][2] = 9;
-  sudoku[4][3] = 1;
-  sudoku[4][4] = 6;
-  sudoku[4][5] = 4;
-  sudoku[4][6] = 5;
-  sudoku[4][7] = 8;
-  sudoku[4][8] = 3;
-  
-  sudoku[5][0] = 5;
-  sudoku[5][1] = 6;
-  sudoku[5][2] = 4;
-  sudoku[5][3] = 9;
-  sudoku[5][4] = 3;
-  sudoku[5][5] = 8;
-  sudoku[5][6] = 2;
-  sudoku[5][7] = 1;
-  sudoku[5][8] = 7;
-  
-  sudoku[6][0] = 2;
-  sudoku[6][1] = 7;
-  sudoku[6][2] = 3;
-  sudoku[6][3] = 5;
-  sudoku[6][4] = 8;
-  sudoku[6][5] = 6;
-  sudoku[6][6] = 1;
-  sudoku[6][7] = 4;
-  sudoku[6][8] = 9;
-  
-  sudoku[7][0] = 4;
-  sudoku[7][1] = 1;
-  sudoku[7][2] = 8;
-  sudoku[7][3] = 3;
-  sudoku[7][4] = 9;
-  sudoku[7][5] = 7;
-  sudoku[7][6] = 6;
-  sudoku[7][7] = 5;
-  sudoku[7][8] = 2;
-  
-  sudoku[8][0] = 9;
-  sudoku[8][1] = 5;
-  sudoku[8][2] = 6;
-  sudoku[8][3] = 4;
-  sudoku[8][4] = 2;
-  sudoku[8][5] = 1;
-  sudoku[8][6] = 7;
-  sudoku[8][7] = 3;
-  sudoku[8][8] = 8;*/
     if(isValid(sudoku))
     {
-      for (int i = 0; i < 9; i++)
-      {
-        for (int j = 0; j < 9; j++)
-        {
-          cout << sudoku[i][j] << " ";
-        }
-        cout << endl;
-      }
-      cout << endl;
+      printSudoku(board);
       counter++;
     }
   }
 }
-
+*/
 int numberOfZeroes(int* array,int arraylength)
 {
   int output = 0;
@@ -229,7 +239,101 @@ int numberOfZeroes(int* array,int arraylength)
     output += (array[i] == 0)?1:0;
   }
 }
-/*
+
+void removeFromBoard(int**& board, int n)
+{
+  for(int i = 0 ; i < 9 ; i++)
+  {
+    for(int j = 0 ; j < 9 ; j++)
+    {
+       if(board[i][j] == n)
+         board[i][j] = 0;
+    }
+  }
+}
+
+
+
+
+void generateBoard(int**& board)
+{
+  int** currentboard = new int*[9];
+  for(int  i = 0; i<9; i++)
+    currentboard[i] = new int[9];
+    
+  int* boardHistoryN = new int[9];
+  
+  
+  vector<int> remainingN;
+  for(int i = 0; i < 9; i++)
+  {
+    remainingN.push_back(i);
+  }
+  
+  
+  int boardHistoryIndex = 0;
+  while(boardHistoryIndex != 9)
+  {
+    int expectedAvailableSlots = (9 - boardHistoryIndex) * 9;
+    
+    vector<int*> availableSlots;
+    int nLoc = rand() % remainingN.size();
+    int chosenN = remainingN.at(nLoc);
+    remainingN.erase(remainingN.begin() + nLoc);
+    getAvaliableSlots(availableSlots, currentboard, chosenN);
+    
+    if(availableSlots.size() != expectedAvailableSlots)
+    {
+      if(remainingN.size() == 1)
+      {
+        for(int  i = 0; i<9; i++)
+        {
+          boardHistoryN[i] = 0;
+          for(int j = 0; j<9; j++)
+            currentboard[i][j] = 0;
+        }
+        boardHistoryIndex = 0;
+        
+        remainingN.clear();
+        for(int i = 0; i < 9; i++)
+        {
+          remainingN.push_back(i);
+        }
+        srand(time(NULL));
+      }
+      else
+      {
+        //cout << "exceeded expected sloots N="<< boardHistoryIndex<<": " << availableSlots.size() << "/" << expectedAvailableSlots << endl;
+        //printSudoku(currentboard);  
+        int rN = boardHistoryN[--boardHistoryIndex];
+        removeFromBoard(currentboard, rN);
+        remainingN.push_back(rN);
+        getAvaliableSlots(availableSlots, currentboard, chosenN);
+      }
+    }
+    
+    do
+    {
+      availableSlots.clear();
+      getAvaliableSlots(availableSlots, currentboard, chosenN);
+      if(availableSlots.size() != 0)
+      {
+        *(availableSlots.at(rand() % availableSlots.size())) = chosenN;
+      }
+    } while(availableSlots.size() != 0);
+    boardHistoryN[boardHistoryIndex++] = chosenN;
+    
+    if(boardHistoryIndex == 9 && !isValid(currentboard))
+    {
+      cout << "Something went wrong!" << endl;
+    }
+  }
+  board = currentboard;
+}
+
+
+
+
 int main()
 {
   int** board;
@@ -244,51 +348,7 @@ int main()
       for(int j = 0; j < 9; j++)
         board[i][j] = 0;
   
-    int x[9] = {0};
-    
-    while((x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7] + x[8]) != 45)
-    {
-      int expectedRemaining = numberOfZeroes(x,int 9)*9;
-      
-      for (int n=1; n<=9; n++)
-      {
-        
-        vector <int*> vect;
-        do// Fill vect
-        {
-          getAvaliableSlots(vect, board, n);
-          if (vect.size() > 0) // if any locations are available...
-          {
-            int randInt = rand() % vect.size(); // choose a random location on the board for n
-            *(vect.at (randInt)) = n; // set chosen location to n
-          }
-          cout<<n<<" ; "<<vect.size()<<endl;
-        }while (vect.size() > 0); // keep iterating until locations arent available for the current n
-      }
-      
-      for (int i = 0; i < 9; i++)
-      {
-        for (int j = 0; j < 9; j++)
-        {
-          cout << board[i][j] << " ";
-        }
-        cout << endl;
-      }
-      
-      cout << endl;
-    }
-  }
-  
-  
-  
-  for (int i = 0; i < 9; i++)
-  {
-    for (int j = 0; j < 9; j++)
-    {
-      cout << board[i][j] << " ";
-    }
-    cout << endl;
+    generateBoard(board);
+    printSudoku(board);
   }
 };
-*/
-
